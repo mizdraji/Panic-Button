9f08877bb851f36342861968f5b06e95ae970e9c
German <mizdraji@gmail.com>
2024-08-30 12:42:13 -0300

    Update Prueba_TASK_SCHEDULER.ino

--- Pruebas/Prueba_TASK_SCHEDULER/Prueba_TASK_SCHEDULER.ino
+++ Pruebas/Prueba_TASK_SCHEDULER/Prueba_TASK_SCHEDULER.ino
@@ -15,7 +15,7 @@
 // #define _TASK_TIMEOUT               // Support for overall task timeout
 // #define _TASK_OO_CALLBACKS          // Support for callbacks via inheritance
 // #define _TASK_EXPOSE_CHAIN          // Methods to access tasks in the task chain
-#define _TASK_SCHEDULING_OPTIONS       // Support for multiple scheduling options
+#define _TASK_SCHEDULING_OPTIONS  // Support for multiple scheduling options
 // #define _TASK_DEFINE_MILLIS         // Force forward declaration of millis() and micros() "C" style
 // #define _TASK_EXTERNAL_TIME         // Custom millis() and micros() methods
 // #define _TASK_THREAD_SAFE           // Enable additional checking for thread safety
@@ -23,55 +23,76 @@
 
 #include <TaskScheduler.h>
 
+
+
 // Scheduler
 Scheduler ts;
 
 /*
    Approach 1: LED is driven by the boolean variable; false = OFF, true = ON
 */
-#define PERIOD1 500
-#define DURATION 10000
+
 void blink1CB();
-Task tBlink1 ( PERIOD1 * TASK_MILLISECOND, DURATION / PERIOD1, &blink1CB, &ts, true );
+Task tBlink1(500, TASK_FOREVER, &blink1CB, &ts, true);
+
+#define buttom 37
 
 
+void IRAM_ATTR panic_show(void) {
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 25) {
+    Serial.println("<----------------------------------------------");
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+//unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;       // Variable para almacenar el tiempo anterior
+const unsigned long ledOnTime = 500;    // Tiempo de encendido en milisegundos (0.5 segundos)
+const unsigned long ledOffTime = 5000;  // Tiempo de apagado en milisegundos (5 segundos)
+bool LED_state = LOW;                   // Estado actual del LED
 
 void setup() {
 
   Serial.begin(115200);
   delay(TASK_SECOND);
   delay(2 * TASK_SECOND);
-#endif
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(buttom, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttom), panic_show, FALLING);
 
+  //currentMillis = millis();
 }
 
 void loop() {
   ts.execute();
+}
 
+inline void LEDOn() {
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+inline void LEDOff() {
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 
 // === 1 =======================================
-bool LED_state = false;
+
 void blink1CB() {
-  if ( tBlink1.isFirstIteration() ) {
-    _PP(millis());
-    _PL(": Blink1 - simple flag driven");
-    LED_state = false;
-  }
+  unsigned long currentMillis = millis();
 
-  if ( LED_state ) {
-    LEDOff();
-    LED_state = false;
-  }
-  else {
-    LEDOn();
-    LED_state = true;
-  }
-
-  if ( tBlink1.isLastIteration() ) {
-    tBlink2.restartDelayed( 2 * TASK_SECOND );
-    LEDOff();
+  if (LED_state == HIGH && (currentMillis - previousMillis >= ledOnTime)) {
+    // Apagar el LED después de 0.5 segundos
+    LED_state = LOW;                  // Cambiar estado del LED
+    previousMillis = currentMillis;   // Actualizar tiempo anterior
+    digitalWrite(LED_BUILTIN, LED_state);  // Apagar el LED
+  } else if (LED_state == LOW && (currentMillis - previousMillis >= ledOffTime)) {
+    // Encender el LED después de 5 segundos
+    LED_state = HIGH;                 // Cambiar estado del LED
+    previousMillis = currentMillis;   // Actualizar tiempo anterior
+    digitalWrite(LED_BUILTIN, LED_state);  // Encender el LED
   }
 }
