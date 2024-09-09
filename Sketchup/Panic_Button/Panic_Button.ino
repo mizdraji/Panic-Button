92531ce9034d851560531ea42bc2acd513ad0af6
German <mizdraji@gmail.com>
2024-09-09 13:13:38 -0300

    V1.3
    Detalle de versiones:
    * V1.3:
    * Se configuran los 3 pulsadores como tareas que se activan a travez de interrupciones. (pendiente mejorar rebote)
    * Se crea el objeto Scheduler interrupt para establecer como alta prioridad
    * Se cambian los nombres de las tareas por t1-t7
    * Se crean las tareas t5, t6 y t7 para interrupciones
    * Se crea estructura de mensajes para continuar con SMS en proxima version.

--- Sketchup/Panic_Button/Panic_Button.ino
+++ Sketchup/Panic_Button/Panic_Button.ino
@@ -1,11 +1,13 @@
 /*Detalle de versiones:
-* V1.2: 
-* se cambia config pines por pinout y se crea funcion config_pines.
-* Se crea Hardware.h y Hardware.ino
-* Se crea funcion pdr para prueba de red pero aun no esta funcional.
+* V1.3: 
+* Se configuran los 3 pulsadores como tareas que se activan a travez de interrupciones. (pendiente mejorar rebote)
+* Se crea el objeto Scheduler interrupt para establecer como alta prioridad
+* Se cambian los nombres de las tareas por t1-t7
+* Se crean las tareas t5, t6 y t7 para interrupciones
 */
 
 //librerias utilizadas
+#define _TASK_PRIORITY
 #include <TaskScheduler.h>          //libreria tareas programadas
 #include "Task.h"                   //librerias tareas a realizar
 #include <lorawan.h>                //libreria lora
@@ -22,12 +24,6 @@
 SoftwareSerial SIM800L(RX, TX);   //RX y TX de heltec
 
 
-
-
-//Scheduler: Este objeto es el programador encargado de la ejecución de las tareas, el cual se tendrá que ejecutar en cada loop
-//Creamos el Scheduler que se encargará de gestionar las tareas
-Scheduler taskManager;
-
 TaskHandle_t Task0;     //Task0 para ejecutar en core0
 
 void setup() {                              //setup run in core1
@@ -43,28 +39,24 @@ void setup() {                              //setup run in core1
   }
   Serial.println("RFM95 detected");
 
-  taskManager.init();
   Serial.println("Initialized scheduler");
-  //taskManager.addTask(TareaLED);           // Añadimos la tarea al programador de tareas
-  //TareaLED.enable();                  // Activamos la tarea
-  taskManager.addTask(SendLora);
-  SendLora.enable();
-  taskManager.addTask(blink2);          //blink cada 10 segundos
-  blink2.enable();
+
+  //Configura Scheduler interrupt como alta prioridad
+  taskManager.setHighPriorityScheduler(&interrupt);
+  taskManager.enableAll(true);  // this will recursively enable the higher priority tasks as well
   
   //Se crea una tarea que se ejecutará en la función loop0(), con prioridad 1 y se ejecutará en el core0.
   xTaskCreatePinnedToCore(loop0, "Task0", 10000, NULL, 1, &Task0, 0);  
-
-  //attachInterrupt(digitalPinToInterrupt(button), panic_show, RISING);
-
 
   if (initLoraTec()) {
   Serial.println("-->LoraTec OK");
   Serial.print("-->devID: TLV2_DPEC_");
   Serial.println(devID);  //Activacion Manual, devID predefinido
   }
-  
-   
+
+  attachInterrupt(digitalPinToInterrupt(button1), buttonInterrupt1, RISING);
+  attachInterrupt(digitalPinToInterrupt(button2), buttonInterrupt2, RISING);
+  attachInterrupt(digitalPinToInterrupt(button3), buttonInterrupt3, RISING);
 }
 
 void loop() {                                           //loop run in core1
