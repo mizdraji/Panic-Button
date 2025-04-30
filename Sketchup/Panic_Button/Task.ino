1f185bef7d4f0e2edd97b18432f12b99f8f1524c
German <mizdraji@gmail.com>
2025-04-30 14:53:30 -0300

    Update Task.ino

--- Sketchup/Panic_Button/Task.ino
+++ Sketchup/Panic_Button/Task.ino
@@ -217,7 +217,6 @@ void informado_led() {
     digitalWrite(led3, LED_state2);
     digitalWrite(led_recibido, LED_state2); 
     digitalWrite(led_atendido, LED_state2);
-    counterInformado ++;  
     timer = 0;
   }
   if (counterInformado > timesCounterInformado) {
