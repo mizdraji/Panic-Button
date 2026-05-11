5f959b3975a31357844660661a78a52112e42788
German <mizdraji@gmail.com>
2026-05-11 09:41:02 -0300

    Update configuracion.h

--- Sketchup/Panic_Button/configuracion.h
+++ Sketchup/Panic_Button/configuracion.h
@@ -20,10 +20,10 @@
 #define FIXED_SF_INDEX  0                      //0:SF7, 1:SF8, 2:SF9, 3:SF10
 
 //configuracion ensayo
-#define MODO_ENSAYO      1                      //1=habilita envio periodico de ensayo
+#define MODO_ENSAYO      1                      //1 = habilita modo ensayo, 0 = modo normal
 #define ENSAYO_INTERVALO_MS 300000UL            //5 minutos
-#define ENSAYO_DURACION_HS 48UL                 //48 horas
-#define ENSAYO_TOTAL_MENSAJES ((ENSAYO_DURACION_HS * 60UL * 60UL * 1000UL) / ENSAYO_INTERVALO_MS) //576 mensajes
+#define ENSAYO_DURACION_HS 24UL                 //48 horas - 24 horas
+#define ENSAYO_TOTAL_MENSAJES ((ENSAYO_DURACION_HS * 60UL * 60UL * 1000UL) / ENSAYO_INTERVALO_MS) //576 mensajes - 288 mensajes
 
 //configuracion GSM numeros de remitentes
 struct remitente {
